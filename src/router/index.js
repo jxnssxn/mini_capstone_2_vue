e827561b4546793c9060ef8f454a679cf330ecba
jimmy <jonssonjimmy8@gmail.com>
2020-12-16 22:00:35 -0500

    create vue login route

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import ProductsIndex from '../views/ProductsIndex.vue';
+import Signup from "../views/Signup.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/Signup',
+    name: 'sign up',
+    component: Signup
+  },
+  {
+    path: '/ProductsIndex',
+    name: 'index',
+    component: ProductsIndex
   },
   {
     path: '/about',
@@ -18,12 +30,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+    <h3>{{ product.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to products index!",
+      products: [],
+      product: {},
+    };
+  },
+  created: function () {
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("products Index...print");
+    },
+  },
+};
+</script>
